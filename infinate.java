3463c4d7bb2ff820805970eb739f8344dcbf4bc7
devikumavath <devikumavath1999@gmail.com>
2021-09-15 20:17:00 +0530

    infinate loop code is added

---
+++ infinate.java
@@ -0,0 +1,13 @@
+
+// largest number out of 3
+import java.util.Scanner;
+
+public class large3 {
+    public static void main(String[] args) {
+     while(true){
+         System.out.println("Devi");
+     }
+
+    }
+
+}
