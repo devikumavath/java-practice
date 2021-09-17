0be9706f25eafdb5e17f4cc54c03148085895416
devikumavath <devikumavath1999@gmail.com>
2021-09-17 17:59:41 +0530

    more java codes 3

---
+++ leapyear.java
@@ -0,0 +1,16 @@
+//find out given year is leap year or not
+// 
+import java.util.Scanner;
+public class leapyear {
+   public static void main(String[] args) {
+     int y;
+     System.out.println("enter any year");
+     Scanner obj=new Scanner(System.in);
+     y=obj.nextInt();
+      if (y%100==0 && y%400==0 || y%100!=0 && y%400==0 ) {
+        System.out.println( y+" " +"is leap year");
+      } else {
+        System.out.println( y+" " +"is not leap year");
+      } 
+   }  
+}
---
+++ positivenegativeno.java
@@ -0,0 +1,21 @@
+
+// check number is postive or negative 
+import java.util.Scanner;
+
+public class positivenegativeno {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("enter any number");
+        Scanner obj = new Scanner(System.in);
+        num = obj.nextInt();
+        if (num > 0) {
+            System.out.println(num + " " + "is postive number");
+        } else if (num < 0) {
+
+            System.out.println(num + "  " + "is negative number");
+        } else {
+            System.out.println(num + "  " + "niether postive nor negative");
+        }
+
+    }
+}
